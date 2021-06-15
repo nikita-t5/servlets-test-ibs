c85ed8cc9a494e02c7a3f17704ca9797cbce052d
nikita-t5 <nickita.tashirev@yandex.ru>
2021-06-15 11:58:08 +0300

    deleted web.xml

--- src/main/java/ru/ibs/taskServlets/main/App.java
+++ src/main/java/ru/ibs/taskServlets/main/App.java
@@ -1,15 +1,19 @@
 package ru.ibs.taskServlets.main;
 
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import ru.ibs.taskServlets.servlets.FirstServlet;
+import ru.ibs.taskServlets.servlets.SecondServlet;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Start main class");
         Server server = new Server(8080);
-        final String webAppPath = App.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-        WebAppContext webapp = new WebAppContext(webAppPath, "/");
-        server.setHandler(webapp);
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        context.addServlet(FirstServlet.class, "/f");
+        context.addServlet(SecondServlet.class, "/s");
+        server.setHandler(context);
         try {
             server.start();
             System.out.println("Server started");
